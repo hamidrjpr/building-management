336ebb9125f67d05307e06c286a797be5aa40bb5
hamidrjpr <hamid.hr1380@gmail.com>
2025-10-18 16:15:22 +0330

    initialize code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ConfigProvider } from "antd";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const Vazir = localFont({
+//   src: [
+//     {
+//       path: "./fonts/Vazir.woff2", // ✅ correct relative path
+//       weight: "400",
+//       style: "normal",
+//     },
+//   ],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body dir="rtl">
+        <ConfigProvider
+          theme={{
+            token: {
+              fontFamily: "Vazir",
+            },
+          }}
+        >
+          {children}
+        </ConfigProvider>
       </body>
     </html>
   );
